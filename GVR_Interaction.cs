d609b08a16a3b66ac79949dcbae315ff62d21dfa
Luciano Paci <lucianopaci93@gmail.com>
2021-04-08 09:03:12 -0300

    Added More Scripts

---
+++ GVR_Interaction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class GVR_Interaction : MonoBehaviour
+{
+
+    public Image img;
+    public UnityEvent GVRClick;
+    bool gvrStatus;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gvrStatus && Input.GetKeyDown("joystick button 4"))
+        {
+            GVRClick.Invoke();
+        }
+    }
+
+
+    public void GvrOn()
+    {
+        gvrStatus = true;
+    }
+    public void GvrOff()
+    {
+        gvrStatus = false;
+    }
+}
---
+++ SceneController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
